afd4b9d1d9eb1ed1a5188b173e66a269c9166b0d
Jamie Kinkead <jkinkead@gmail.com>
2019-02-12 14:42:24 -0800

    letters timed, answers clear

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { newGame } from '../actions/games'
-import { Input, Button, Segment, Header } from 'semantic-ui-react'
-// import moment from 'moment'
-import moment from 'moment-timer' 
+import { Input, Button, Segment, Header, Transition } from 'semantic-ui-react'
+
 
 
 
@@ -17,7 +16,8 @@ class Game extends Component {
             four: [],
             five: [],
             six: [],
-            seven: []
+            seven: [],
+            visible: false
         }
     }
 
@@ -64,16 +64,21 @@ class Game extends Component {
 
     scoreGame = (e) => {
         console.log("scoring")
+        this.setState({
+            visible: false,
+            three: [],
+            four: [],
+            five: [],
+            six: [],
+        })
     }
 
     playGame = () => {
         this.requestNewGame()
+        this.setState({
+            visible: true
+        })
         setInterval(this.scoreGame, 60000)
-    }
-
-    playGame = () => {
-        let timer = new moment.duration(3000).timer({ start: true }, this.requestNewGame)
-        timer.stop()
     }
 
 
@@ -81,12 +86,13 @@ class Game extends Component {
         return (
             <div>
                 <Segment>
-                    <Header as='h1'>
-                        {/* make more random? use ._shuffle? */}
-                        {this.props.playletters ?
-                            this.props.playletters.split('').sort(function () { return 0.5 - Math.random() }).join('')
-                            : null}
-                    </Header>
+                    <Transition visible={this.state.visible} animation='scale' duration={500}>
+                        <Header as='h1'>
+                            {this.props.playletters ?
+                                this.props.playletters.split('').sort(function () { return 0.5 - Math.random() }).join('')
+                                : null}
+                        </Header>
+                    </Transition>
                 </Segment>
                 <form onSubmit={this.handleGuess}>
                     <Input type="text" onChange={this.handleChange} value={this.state.guess} name="guess" placeholder='Guess Here' />
@@ -96,7 +102,7 @@ class Game extends Component {
                     <Button onClick={this.playGame} basic color='orange' content='NEW GAME' />
                 </Segment>
                 <Segment>
-                    <Button onClick={this.handleSubmit} basic color='teal' content='SCORE GAME' />
+                    <Button onClick={this.scoreGame} basic color='teal' content='SCORE GAME' />
                 </Segment>
                 <Segment>
                     <Header as='h2'>
