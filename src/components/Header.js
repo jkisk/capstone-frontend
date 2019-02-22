1a5435450ec6608021ff519c690fdcb404670c99
Jamie Kinkead <jkinkead@gmail.com>
2019-02-22 12:54:58 -0800

    removed svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Image} from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
-// import {Logo} from './Logo'
 import mainLogo from '../small_scram.png'
 
 
